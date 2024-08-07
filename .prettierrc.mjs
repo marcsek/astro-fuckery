3a873bcf146edb30ac2aa0d458971145fb969013
marcsek <jakubmarcek955@gmail.com>
2024-08-07 20:53:18 +0200

    Initial

---
+++ .prettierrc.mjs
@@ -0,0 +1,20 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-astro'],
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 140,
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
+
+export default config;
